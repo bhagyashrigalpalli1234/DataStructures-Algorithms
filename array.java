d6f394a4baedf0ad38b559981d43ce764f8cff3d
bhagyashrigalpalli1234 <47694999+bhagyashrigalpalli1234@users.noreply.github.com>
2022-12-18 12:42:08 +0530

    Update array.java

--- array.java
+++ array.java
@@ -1,3 +1,11 @@
+//Given an array of size N containing only 0s, 1s, and 2s; sort the array in ascending order.
+
+---approach1---
+    1.sort array   Tc-O(N log(N))  Sc-O(1)
+    2.count 0,1,2  Tc-O(2N)
+    3.Dutch national flag algorithm  Tc-O(N) Sc-O(1)
+
+
 import java.util.Scanner;
 public class array {
     public static void main(String args[])
