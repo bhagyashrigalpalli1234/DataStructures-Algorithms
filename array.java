8de9d515842ce1bb4961129df1d887130a60ba1b
bhagyashrigalpalli1234 <47694999+bhagyashrigalpalli1234@users.noreply.github.com>
2023-01-01 20:07:46 +0530

    Update array.java

--- array.java
+++ array.java
@@ -70,3 +70,30 @@ class Reverse
     
 }
 
+2.find max and min and return sum of them
+
+class Solution
+{ 
+    public static int findSum(int A[],int N) 
+    {
+        int min=A[0],max=A[0],sum=0;
+        for(int i=0;i<N;i++)
+        {
+            if(A[i]>max)
+            {
+                max=A[i];
+            }
+            if(A[i]<min)
+            {
+                min=A[i];
+            }
+            
+            
+        }
+        
+        sum=min+max;
+        return sum;
+    }
+}
+
+    
